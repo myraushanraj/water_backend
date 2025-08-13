de7f7fb50be6aaf37059b4993b47748648078aa3
Lakshman <lkashyap92@gmail.com>
2025-08-13 10:11:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -18,11 +18,16 @@ await connectToDatabase();
 const app = express();
 
 // Security headers
-app.use(
-  helmet({
-    crossOriginResourcePolicy: { policy: 'cross-origin' },
-  })
-);
+// app.use(
+//   helmet({
+//     crossOriginResourcePolicy: { policy: 'cross-origin' },
+//   })
+// );
+
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(morgan('dev'));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
