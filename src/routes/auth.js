eb4baa0802998b9328f2b84a7b28db45d535775a
raushanIarc <raushan.raj@iarc.co.in>
2025-08-14 20:17:59 +0530

    done

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,7 @@ router.post('/register', [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Valid email is required'),
   body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
-  body('phone').notEmpty().withMessage('Phone number is required').isMobilePhone().withMessage('Valid phone number is required')
+  body('phone').notEmpty().withMessage('Phone number is required')
 ], register);
 router.post('/login', [body('email').isEmail(), body('password').isLength({ min: 6 })], login);
 router.post('/logout', logout);
